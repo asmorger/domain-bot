360876694f5d8a0f078c27cecfe2b6a78e2da574
Andrew Morger <andrew@getparallax.com>
2021-12-30 10:05:18 -0600

    Adds tests data for valid DSL entries

--- src/DBot.Tests/Parsing/ExpressionParserTests.cs
+++ src/DBot.Tests/Parsing/ExpressionParserTests.cs
@@ -1,4 +1,5 @@
 ﻿using DBot.Dsl.Parsing;
+using DBot.Tests.SampleData;
 using FluentAssertions;
 using Xunit;
 
@@ -6,45 +7,11 @@ namespace DBot.Tests.Parsing;
 
 public class ExpressionParserTests
 {
-    [Fact]
-    public void Basic_system_definition_parses_succesfully()
+    [Theory]
+    [ValidDsls]
+    public void Tokenizer_understands_known_dsls(string dsl)
     {
-        var tokens = ExpressionTokenizer.TryTokenize("system GizmoMaker { }");
-
-        tokens.HasValue.Should().BeTrue();
-
-        var result = ExpressionParser.TryParse(tokens.Value, out var expr, out var error, out var errorPosition);
-
-        result.Should().BeTrue();
-        expr.Should().NotBeNull();
-        error.Should().BeNull();
-    }
-    
-    [Fact]
-    public void Basic_system_definition__with_empty_children_parses_succesfully()
-    {
-        const string dsl = @"system GizmoMaker { 
-    aggregate Whatzit { 
-        entity Whozit { },
-        entity Thingamajig { }
-    },
-    value Dohickey { }
-}";
         var tokens = ExpressionTokenizer.TryTokenize(dsl);
-
-        tokens.HasValue.Should().BeTrue();
-
-        var result = ExpressionParser.TryParse(tokens.Value, out var expr, out var error, out var errorPosition);
-
-        result.Should().BeTrue();
-        expr.Should().NotBeNull();
-        error.Should().BeNull();
-    }
-    
-    [Fact]
-    public void Complex_system_definition_parses_succesfully()
-    {
-        var tokens = ExpressionTokenizer.TryTokenize("system \"Gizmo Maker\" { }");
 
         tokens.HasValue.Should().BeTrue();
 
