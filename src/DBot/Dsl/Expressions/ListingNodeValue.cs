0df6dc99ae9047fc7916e9ddc294fe0694820a8f
Andrew Morger <andrew@getparallax.com>
2021-12-30 22:01:47 -0600

    Adds expression for id/children only structure

---
+++ src/DBot/Dsl/Expressions/ListingNodeValue.cs
@@ -0,0 +1,17 @@
+﻿namespace DBot.Dsl.Expressions;
+
+public class ListingNodeValue : Expression
+{
+    public ListingNodeValue(IdentifierValue identifier, Expression[] children)
+    {
+        Identifier = identifier;
+        Children = children;
+    }
+
+    public IdentifierValue Identifier { get; init; }
+    public Expression[] Children { get; init; }
+
+    public bool HasChildren => Children.Any();
+
+    public override string ToString() => $"{Identifier}with {Children.Length} items";
+}
