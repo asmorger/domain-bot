45a847004077e503b36bd143eaf9f85a4fc60338
Andrew Morger <andrew@getparallax.com>
2021-12-30 22:01:09 -0600

    Adds event domain representation

---
+++ src/DBot/Domain/EventListing.cs
@@ -0,0 +1,26 @@
+﻿namespace DBot.Domain;
+
+public class EventListing : BaseHierarchicalCodeElement
+{
+    public EventListing() : base("events")
+    {
+    }
+
+    public EventListing(IEnumerable<CodeElement> elements) : this()
+    {
+        foreach (var element in elements)
+        {
+            AddChild(element);
+        }
+    }
+}
+
+public class Event : CodeElement
+{
+    public string Name { get; }
+
+    public Event(string name)
+    {
+        Name = name;
+    }
+}
