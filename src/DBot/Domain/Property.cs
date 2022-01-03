46ea1c4ac3df20da3d11f6095c04fa3545662a05
Andrew Morger <andrew@getparallax.com>
2022-01-03 11:22:13 -0600

    Adds support for declaring properties

---
+++ src/DBot/Domain/Property.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace DBot.Domain;
+
+[DebuggerDisplay("Property: {Name}")]
+public class Property : CodeElement
+{
+    public Property(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}
+
+[DebuggerDisplay("properties")]
+public class PropertyListing : BaseHierarchicalCodeElement
+{
+    public PropertyListing() : base("properties")
+    {
+    }
+
+    public PropertyListing(IEnumerable<CodeElement> elements) : this()
+    {
+        foreach (var element in elements)
+        {
+            AddChild(element);
+        }
+    }
+}
