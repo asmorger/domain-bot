50391c116c57d5adc8b0d1210400b247df163a56
Andrew Morger <andrew@getparallax.com>
2022-01-04 00:40:37 -0600

    Adds Service keyword

--- src/DBot/Dsl/Parsing/Parsers/UniversalParsers.cs
+++ src/DBot/Dsl/Parsing/Parsers/UniversalParsers.cs
@@ -8,14 +8,15 @@ using ExpressionTokenParser = TokenListParser<ExpressionToken, Expression>;
 public static class UniversalParsers
 {
     public static ExpressionTokenParser Keyword { get; } =
-        Token.EqualTo(ExpressionToken.System)
-            .Or(Token.EqualTo(ExpressionToken.Aggregate))
+        Token.EqualTo(ExpressionToken.Aggregate)
             .Or(Token.EqualTo(ExpressionToken.Behavior))
             .Or(Token.EqualTo(ExpressionToken.Description))
             .Or(Token.EqualTo(ExpressionToken.Entity))
             .Or(Token.EqualTo(ExpressionToken.Events))
             .Or(Token.EqualTo(ExpressionToken.Properties))
             .Or(Token.EqualTo(ExpressionToken.Raises))
+            .Or(Token.EqualTo(ExpressionToken.Service))
+            .Or(Token.EqualTo(ExpressionToken.System))
             .Or(Token.EqualTo(ExpressionToken.ValueObject))
             .Apply(ExpressionTextParsers.Keyword)
             .Select(id => (Expression) new KeywordValue(id));
