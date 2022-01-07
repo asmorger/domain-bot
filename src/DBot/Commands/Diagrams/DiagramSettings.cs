4365a17170a0c2368bacfa34c938558319e0ba44
Andrew Morger <andrew@getparallax.com>
2022-01-07 05:20:54 -0600

    Begins adding support for diagram generation

---
+++ src/DBot/Commands/Diagrams/DiagramSettings.cs
@@ -0,0 +1,14 @@
+﻿using DBot.Commands.Common;
+using Spectre.Console.Cli;
+
+namespace DBot.Commands.Diagrams;
+
+public class DiagramSettings : SourceFileSettings
+{
+    [CommandOption("-f|--format")] public DiagramFormat DiagramFormat { get; set; } = DiagramFormat.Mermaid;
+}
+
+public enum DiagramFormat
+{
+    Mermaid
+}
---
+++ src/DBot/Commands/Diagrams/EntityRelationshipDiagram.cs
@@ -0,0 +1,10 @@
+﻿using DBot.Commands.Common;
+using DBot.Domain;
+using Spectre.Console;
+
+namespace DBot.Commands.Diagrams;
+
+public class EntityRelationshipDiagram : DslCommand<DiagramSettings>
+{
+    protected override void Process(CodeElement system) => AnsiConsole.WriteLine("ER Diagrams will go here.");
+}
